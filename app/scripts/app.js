c2efcfc10db79aa9beccec255d581c77144e95dd
BenjaminDuv <bduvivier.ext@orange.com>
2014-06-02 17:12:13 +0100

    Footer update + Contact us smooth scroll linked implemented

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,3 +3,10 @@
 $('#contactButton').click(function(){
 	alert('Go to the footpage');
 });
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
